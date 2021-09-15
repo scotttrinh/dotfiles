06a64f7888f5e9a6fa8475a947a29f38de52620c
Scott Trinh <scott@scotttrinh.com>
2021-09-15 14:14:21 -0400

    Fix reference to removed variable

--- scott/modules/scott/default.nix
+++ scott/modules/scott/default.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, scott, home-manager, nix-darwin, nixpkgs, ... }@args:
 {
-  home-manager.users.${USER} = {
+  home-manager.users.scotttrinh = {
     home.packages = config.pkgSets.scott;
   };
 }
