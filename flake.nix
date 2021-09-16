02de274f6b87446fc39530c9b1425645ef00ae6a
Scott Trinh <scott@scotttrinh.com>
2021-09-15 21:13:18 -0400

    Squashed commit of the following:
    commit 378d12884d65e2c7b580a3a0b83b3bb615322d08
    Author: Scott Trinh <scott@scotttrinh.com>
    Date:   Wed Sep 15 21:12:01 2021 -0400
    
        Working! Needed to enable zsh.

--- flake.nix
+++ flake.nix
@@ -11,9 +11,38 @@
   };
 
   outputs = { self, mk-darwin-system, nixpkgs, ... }@inputs:
-    import ./scott (mk-darwin-system.inputs // {
-      scott = self;
-      inherit nixpkgs;
-      inherit (mk-darwin-system) mkDarwinSystem;
-    });
+    let
+      flake-utils = mk-darwin-system.inputs.flake-utils;
+      hostName = "cala-2021-mbp-13";
+      systems = [ "aarch64-darwin" ];
+    in flake-utils.lib.eachSystem systems (system:
+      mk-darwin-system.mkDarwinSystem {
+        inherit hostName system;
+
+        nixosModules = [
+          ({ pkgs, ... }: {
+            system.stateVersion = 4;
+            programs.zsh.enable = true;
+            services.nix-daemon.enable = true;
+            nix.package = pkgs.nixFlakes;
+            nix.extraOptions = ''
+              system = aarch64-darwin
+              extra-platforms = aarch64-darwin x86_64-darwin
+              experimental-features = nix-command flakes
+              build-users-group = nixbld
+            '';
+            environment.systemPackages = with pkgs; [ nixFlakes home-manager ];
+            users.users.scotttrinh.home = "/Users/scotttrinh";
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.scotttrinh = {
+              home.packages = with pkgs; [ ripgrep ];
+            };
+          })
+        ];
+
+        flakeOutputs = { pkgs, ... }@outputs:
+          outputs // (with pkgs; { packages = { inherit hello; }; });
+
+      });
 }
--- scott/modules/scott/default.nix
+++
@@ -1,6 +0,0 @@
-{ config, pkgs, lib, scott, home-manager, nix-darwin, nixpkgs, ... }@args:
-{
-  home-manager.users.scotttrinh = {
-    home.packages = config.pkgSets.scott;
-  };
-}
