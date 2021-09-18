9d2be0716f128a95663d5b29f05273cb720543ff
Scott Trinh <scott@scotttrinh.com>
2021-09-18 14:37:18 -0400

    Add homebrew integration for non-nix packages

--- flake.nix
+++ flake.nix
@@ -89,6 +89,31 @@
                 bat
               ];
             };
+
+            homebrew = {
+              brewPrefix = "/opt/homebrew/bin";
+              enable = true;
+              autoUpdate = true;
+              cleanup = "zap";
+              global = {
+                brewfile = true;
+                noLock = true;
+              };
+
+              taps = [
+                "homebrew/core"
+                "homebrew/cask"
+              ];
+
+              casks = [
+                "firefox"
+              ];
+
+              masApps = {
+                "1Password" = 1333542190;
+                "Dato" = 1470584107;
+              };
+            };
           })
         ];
 
