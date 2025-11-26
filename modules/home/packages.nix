4e6b34dff769eea8cdecd17bb6886a5427ea21f5
Scott Trinh <scott@scotttrinh.com>
2025-11-25 21:12:03 -0500

    Prune the packages a bit

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -1,69 +1,16 @@
 { flake, pkgs, ... }:
 let
   packages = flake.inputs.self + /packages;
-  uncloud = pkgs.callPackage "${packages}/uncloud.nix" { };
   ai-tools = flake.inputs.nix-ai-tools.packages.${pkgs.system};
 in {
   # Nix packages to install to $HOME
   #
   # Search for packages here: https://search.nixos.org/packages
   home.packages = with pkgs; [
-    (pkgs.buildGoModule {
-      pname = "vimeo-dl";
-      version = "0.2.0";
-
-      src = pkgs.fetchFromGitHub {
-        owner = "akiomik";
-        repo = "vimeo-dl";
-        rev = "v0.2.0";
-        sha256 = "sha256-Ys1gFRi/9LftbAoW/wbkmh5wf+KxqgKSrKHExHtKIkg=";
-      };
-
-      vendorHash = "sha256-eKeUhS2puz6ALb+cQKl7+DGvm9Cl+miZAHX0imf9wdg=";
-
-      meta = {
-        description = "A simple command line tool for downloading videos from Vimeo";
-        homepage = "https://github.com/akiomik/vimeo-dl";
-        license = pkgs.lib.licenses.asl20;
-      };
-    })
-    (pkgs.writeShellApplication {
-      name = "edgedb-destroy-local-instances";
-      runtimeInputs = [ pkgs.jq ];
-      text = ''
-        instances=$(edgedb instance list --json)
-        local_instances=$(echo "$instances" | jq -r '.[] | select(has("service-status")) | .name')
-
-        if [ -z "$local_instances" ]; then
-          echo "No local instances to destroy."
-          exit 0
-        fi
-
-        echo "The following local instances will be destroyed:"
-        echo "$local_instances"
-        echo
-
-        read -r -p "Are you sure you want to destroy these instances? (y/N): " confirm
-        if [[ "$confirm" != "y" ]]; then
-          echo "Aborted."
-          exit 0
-        fi
-
-        for instance in $local_instances; do
-          echo "Destroying local instance: $instance"
-          edgedb instance destroy --force -I "$instance"
-        done
-
-        echo "All selected local instances destroyed."
-      '';
-    })
     age
     sops
     ffmpeg
     unrar
-    cloudflared
-    yt-dlp
-    ocamlPackages.ocaml-lsp
     nodejs_24
     corepack_24
     python312
