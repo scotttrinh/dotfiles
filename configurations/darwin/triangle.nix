7a5d37723175332a0707f364b397189749afbf60
Scott Trinh <scott@scotttrinh.com>
2025-12-01 09:34:14 -0500

    Add a few macos apps to triangle

--- configurations/darwin/triangle.nix
+++ configurations/darwin/triangle.nix
@@ -28,4 +28,11 @@ in
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = 4;
+
+  homebrew = {
+    casks = [
+      "1password"
+      "slack"
+    ];
+  };
 }
