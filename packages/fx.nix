c48930e33cf6aadb94f46cf81311f96dc45059ba
Scott Trinh <scott@scotttrinh.com>
2026-08-20 14:00:49 -0400

    Bump llm-agents and fx

--- packages/fx.nix
+++ packages/fx.nix
@@ -9,26 +9,26 @@ let
     {
       aarch64-darwin = {
         name = "macos-aarch64";
-        hash = "sha256-pEDfWSjWuDDSnoZ5DBeNMID+8+B6tzZbdk05m1MwujE=";
+        hash = "sha256-OVrDgy9vbCMfa6eka6bscO763baGYub9bE+44NbXL1k=";
       };
       x86_64-darwin = {
         name = "macos-x86_64";
-        hash = "sha256-w/JivtLfyLSkRJL/POwYUOyIeZqTGxGxSrxOiV+SW2k=";
+        hash = "sha256-QdPCzXi9tTqp3xb71a6UFcii4+iFHr5kI9sMwyEov3w=";
       };
       aarch64-linux = {
         name = "linux-aarch64";
-        hash = "sha256-Md4JbIZYatTpLSi3qKSqPhQqK0E6oKAgkdLqWcESjAU=";
+        hash = "sha256-mQWlHCE9G3/jtQefAP0+YfLbpb3nBzl5kelTXEpwDK8=";
       };
       x86_64-linux = {
         name = "linux-x86_64";
-        hash = "sha256-LX9XhBR80FESAHuqgqWhs9ubn/U3aeR8k+gDwieccxc=";
+        hash = "sha256-vpQoY2r7EZbLZitI7Ve77TuV58N/K8eEngLAlg+uHwE=";
       };
     }.${stdenvNoCC.hostPlatform.system}
       or (throw "fx is not supported on ${stdenvNoCC.hostPlatform.system}");
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "fx";
-  version = "0.0.2";
+  version = "0.0.4";
 
   src = fetchurl {
     url = "https://ugiwefobuo4tac0m.public.blob.vercel-storage.com/cli/v${finalAttrs.version}/fx-${platform.name}.tar.gz";
