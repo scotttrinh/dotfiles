bcc5dbacf3ac2fb2ddc897829e9d7e2960935487
Scott Trinh <scott@scotttrinh.com>
2026-08-31 16:03:02 -0400

    Add "org-today" custom command
    Easily jump to today's weekly file

--- config/doom/base/modules/org.el
+++ config/doom/base/modules/org.el
@@ -9,6 +9,28 @@
   (when (file-directory-p directory)
     (directory-files-recursively directory "\\.org\\'")))
 
+(defun scott/org-today ()
+  "Visit today's heading in the current ISO week file."
+  (interactive)
+  (let* ((week (format-time-string "%G-w%V"))
+         (file (expand-file-name
+                (format "weekly/%s__weekly_work_family.org" week)
+                org-directory))
+         (heading (format-time-string "%A %Y-%m-%d")))
+    (unless (file-exists-p file)
+      (user-error "Weekly file does not exist: %s" file))
+    (find-file file)
+    (goto-char (point-min))
+    (unless (re-search-forward
+             (format "^\\* %s[ \t]*$" (regexp-quote heading)) nil t)
+      (user-error "Today's heading does not exist: %s" heading))
+    (beginning-of-line)
+    (org-reveal)))
+
+(map! :leader
+      (:prefix ("n" . "notes")
+       :desc "Today's daily" "d" #'scott/org-today))
+
 (after! org
   (setq org-default-notes-file (expand-file-name "inbox.org" org-directory)
         org-agenda-files (or (scott/org-files-under-directory org-directory)
