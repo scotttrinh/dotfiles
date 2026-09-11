b41f1b3fe7a453340bcea8cda4873f1854418929
Scott Trinh <scott@scotttrinh.com>
2026-09-11 15:19:48 -0400

    Bump llm-agents

--- packages/fx.nix
+++ packages/fx.nix
@@ -9,19 +9,19 @@ let
     {
       aarch64-darwin = {
         name = "macos-aarch64";
-        hash = "sha256-nkiXyy4J/ff5qhN1lOZCZNLX5Buy30bpVZ9882vkk3s=";
+        hash = "sha256-AZ2S5K97Z2jcs2cytSa7Qj7p6CSa/oKod9Gww6mSgH0=";
       };
       x86_64-darwin = {
         name = "macos-x86_64";
-        hash = "sha256-l+ev5AQ7uzSQoiveA+JhEMHii25pBZvaN2vkrdw8kzE=";
+        hash = "sha256-PTnH8N1JbzlOhjLVd9SSQlb4GoW/b4z0Q5NYt/wRGN8=";
       };
       aarch64-linux = {
         name = "linux-aarch64";
-        hash = "sha256-FvszMN5PKHTlf7d3hC57bEEyyp2xw9l9Vf/8XnRSxA4=";
+        hash = "sha256-g7KINfAU6ArSwNy5evFyUzarfSf4n6Zh7MgwN+Fa+h8=";
       };
       x86_64-linux = {
         name = "linux-x86_64";
-        hash = "sha256-34pcBuCkmpQ3RXrfE/izj/EaNnmkYxBc8yv3V6s3p2E=";
+        hash = "sha256-aFKYWwnpby+vwdYEpfC+e572PE3caM9zNZsuzsim3wo=";
       };
     }
     .${stdenvNoCC.hostPlatform.system}
@@ -30,7 +30,7 @@ in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "fx";
   version = "0.0.8";
-  rev = "e8aa9133417f8b8d5228ceb1ba64cc74dad33db1";
+  rev = "69d0e9610c7958ae2429d3a1ff40a509e3340781";
 
   src = fetchurl {
     url = "https://releases.fx.sh/dev/${finalAttrs.rev}/fx-${platform.name}.tar.gz";
